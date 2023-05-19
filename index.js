83670df410025c45c500ebd98f90cdd6c247142c
RianOntu <rianontu13@gmail.com>
2023-05-20 03:47:33 +0600

    created server side

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const port=5000;
+
+app.use(cors());
+const chefs=require("./data/chef.json")
+
+
+app.get('/',(req,res)=>{
+    res.send("Server is running")
+})
+
+app.get('/chefs',(req,res)=>{
+    res.send(chefs);
+})
+
+app.get('/chefs/:id',(req,res)=>{
+    const id=req.params.id;
+    const chef=chefs.find(n=>n.id==id);
+    res.send(chef)
+})
+
+app.listen(port);
